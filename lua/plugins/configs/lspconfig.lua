8419697026f74ac6ba9f598138cdcdd6e716d7b3
cgfork <jamesrily686@gmail.com>
2023-08-16 19:37:23 +0800

    markdown

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -79,6 +79,11 @@ lspconfig.tsserver.setup {
     root_dir = lspconfigutil.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
 }
 
+lspconfig.marksman.setup({
+    filetypes = { "md", "markdown" },
+    root_dir = lspconfigutil.root_pattern(".git", ".marksman.toml"),
+})
+
 local sign = function(opts)
     vim.fn.sign_define(opts.name, {
         texthl = opts.name,
