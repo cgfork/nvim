deb40224d4fa05f107d58b04e54969a7e600e6b6
cgfork <jamesrily686@gmail.com>
2023-08-17 18:27:49 +0800

    copilot setup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -348,6 +348,24 @@ local plugins_to_install = {
 
     {
         "github/copilot.vim",
+        config = function()
+            vim.g.copilot_no_tab_map = true
+            vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+            vim.g.copilot_filetypes = {
+                ["*"] = false,
+                ["javascript"] = true,
+                ["typescript"] = true,
+                ["lua"] = false,
+                ["rust"] = true,
+                ["c"] = true,
+                ["c#"] = true,
+                ["c++"] = true,
+                ["go"] = true,
+                ["markdown"] = true,
+                ["python"] = true,
+            }
+        end
     },
 
     {
