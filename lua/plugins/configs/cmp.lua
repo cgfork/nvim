0843f9f722c4fca29f117fa6cb879a714a01a432
cgfork <jamesrily686@gmail.com>
2023-08-17 18:20:00 +0800

    lspkind

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -4,13 +4,16 @@ local cmp = require("cmp")
 require("luasnip/loaders/from_vscode").lazy_load()
 
 vim.opt.completeopt = "menu,menuone,noselect"
-
-cmp.setup {
+local opts = {
     snippet = {
         expand = function(args)
             luasnip.lsp_expand(args.body)
         end,
     },
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+    }),
     mapping = cmp.mapping.preset.insert({
         ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
         ['<C-d>'] = cmp.mapping.scroll_docs(4),  -- Down
@@ -39,9 +42,36 @@ cmp.setup {
             end
         end, { 'i', 's' }),
     }),
-    sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-    },
+
 }
+
+local status, lspkind = pcall(require, 'lspkind')
+if status then
+    opts.formatting = {
+        format = lspkind.cmp_format({
+            mode = 'symbol',
+            maxwidth = 50,
+            ellipsis_char = '...',
+
+            before = function(entry, vim_item)
+                if vim_item.kind == 'Color' and entry.completion_item.documentation then
+                    local _, _, r, g, b = string.find(entry.completion_item.documentation, '^rgb%((%d+), (%d+), (%d+)')
+                    if r then
+                        local color = string.format('%02x', r) .. string.format('%02x', g) .. string.format('%02x', b)
+                        local group = 'Tw_' .. color
+                        if vim.fn.hlID(group) < 1 then
+                            vim.api.nvim_set_hl(0, group, { fg = '#' .. color })
+                        end
+                        vim_item.kind = "●"
+                        vim_item.kind_hl_group = group
+                        return vim_item
+                    end
+                end
+                vim_item.kind = lspkind.symbolic(vim_item.kind) and lspkind.symbolic(vim_item.kind) or vim_item.kind
+                return vim_item
+            end
+        })
+    }
+end
+cmp.setup(opts)
 cmp.event:on('confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done())
