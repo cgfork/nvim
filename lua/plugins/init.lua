58a130a4c271b1ecf4c62354658d35902b653322
cgfork <jamesrily686@gmail.com>
2023-10-08 14:54:52 +0800

    bufferline

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -389,12 +389,35 @@ local plugins_to_install = {
     },
     {
         "lukas-reineke/indent-blankline.nvim",
-        opts = {
-            show_current_context = true,
-            show_current_context_start = true,
-            show_end_of_line = true,
-            space_char_blankline = " ",
-        },
+        main = "ibl",
+        config = function()
+            -- local hooks = require "ibl.hooks"
+            -- create the highlight groups in the highlight setup hook, so they are reset
+            -- every time the colorscheme changes
+            -- hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+            --     vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+            --     vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+            --     vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+            --     vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+            --     vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+            --     vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+            --     vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+            -- end)
+            -- require("ibl").setup({
+            --     indent = {
+            --         highlight = {
+            --             "RainbowRed",
+            --             "RainbowYellow",
+            --             "RainbowBlue",
+            --             "RainbowOrange",
+            --             "RainbowGreen",
+            --             "RainbowViolet",
+            --             "RainbowCyan",
+            --         }
+            --     },
+            -- })
+            require("ibl").setup {}
+        end
     },
 
     {
@@ -421,10 +444,11 @@ local plugins_to_install = {
 
     {
         "akinsho/bufferline.nvim",
+        version = "*",
         dependencies = {
             "nvim-tree/nvim-web-devicons",
         },
-        enabled = false,
+        enabled = true,
         opts = {
             options = {
                 diagnostics = "nvim_lsp",
