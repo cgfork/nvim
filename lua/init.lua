b9d0c45269856fe8a882982fc1b57ab8bb20c398
yiwen <5365938+cgfork@users.noreply.github.com>
2025-05-13 07:24:00 +0000

    neovim 11

--- lua/init.lua
+++ lua/init.lua
@@ -35,41 +35,15 @@ if not vim.loop.fs_stat(lazypath) then
     lazy_install(lazypath)
 end
 
-
--- Font for neovide
-if vim.g.neovide then
-    if vim.loop.os_uname().sysname == "Linux" then
-        vim.o.guifont = "FiraMono Nerd Font:h11"
-    elseif vim.loop.os_uname().sysname == "Windows_NT" then
-        vim.o.guifont = "Hack Nerd Font:h10"
-    else
-        vim.o.guifont = "Hack Nerd Font:h12"
-    end
-    vim.o.linespace = 0
-    vim.g.neo_scale_factor = 1.0
-elseif vim.loop.os_uname().sysname == "Windows_NT" then
-    vim.o.guifont = "Consolas NF:h10"
-end
-
 vim.opt.rtp:prepend(lazypath)
 
 if vim.g.vscode then
     Echo "Loading nvim from vscode."
 else
-    try_require("plugins", true)
-end
-
--- Colorscheme for neovide
-if not vim.g.neovide then
-    local status, _ = pcall(require, "catppuccin")
-    if status then
-        vim.cmd("set background=dark")
-        vim.cmd("colorscheme catppuccin")
-    end
-else
-    local status, _ = pcall(require, "catppuccin")
-    if status then
-        vim.cmd("set background=dark")
-        vim.cmd("colorscheme catppuccin")
-    end
+    require('lazy').setup({
+        spec = {
+            { import = "plugins_" },
+        }
+    })
+    require('core/lspconfig')
 end
