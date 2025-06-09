ec7aafa6e89bec05319f5c880aed9dc655f41d0e
yiwen <jamesrily686@gmail.com>
2025-06-09 13:26:14 +0800

    min width

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -6,11 +6,16 @@ return {
     },
     config = function()
         require("aerial").setup({
+            layout = {
+                max_width = { 60, 0.2 },
+                width = nil,
+                min_width = 20,
+            },
             on_attach = function(bufnr)
                 vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr, desc = '[{] Aerial Prev' })
                 vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr, desc = '[}] Aerial Next' })
             end
         })
-        vim.keymap.set('n', '<leader>ut', '<cmd>AerialToggle!<CR>', { desc = 'Aerial Toggle' })
+        vim.keymap.set('n', '<leader>ot', '<cmd>AerialToggle!<CR>', { desc = 'Aerial Toggle' })
     end,
 }
