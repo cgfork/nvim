883a64162080efe173e46577fd9f24d2912c47ce
cgfork <jamesrily686@gmail.com>
2025-06-30 20:15:59 +0800

    fidget

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -2,4 +2,26 @@ return {
     "folke/trouble.nvim",
     -- opts will be merged with the parent spec
     opts = { use_diagnostic_signs = true },
+    keys = {
+        {
+            "<leader>td",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Trouble diagnostics toggle",
+        },
+        {
+            "<leader>ts",
+            "<cmd>Trouble symbols toggle focus=false<cr>",
+            desc = "Trouble symbols toggle",
+        },
+        {
+            "<leader>tl",
+            "<cmd>Trouble loclist toggle<cr>",
+            desc = "Trouble location list",
+        },
+        {
+            "<leader>tq",
+            "<cmd>Trouble qflist toggle<cr>",
+            desc = "Trouble quickfix list",
+        }
+    }
 }
