b5324b6a89cadc750c4c7cab0cff6590e0de6bd3
yiwen <jamesrily686@gmail.com>
2026-02-28 10:36:50 +0800

    remove on_attach for nvim-tree

--- lua/init.lua
+++ lua/init.lua
@@ -1,37 +1,42 @@
 require("core")
 
 function Echo(str)
-    vim.cmd "redraw"
-    vim.api.nvim_echo({ { str, "Bold" } }, true, {})
+	vim.cmd("redraw")
+	vim.api.nvim_echo({ { str, "Bold" } }, true, {})
 end
 
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 local function lazy_install(path)
-    Echo "Installing lazy.nvim & plugins ..."
-    local repo = "https://github.com/folke/lazy.nvim.git"
-    vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", repo, path }
-    vim.opt.rtp:prepend(path)
+	Echo("Installing lazy.nvim & plugins ...")
+	local repo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", repo, path })
+	vim.opt.rtp:prepend(path)
 
-    require("plugins")
+	require("plugins")
 
-    -- The hook for setup
+	-- The hook for setup
 end
 
 Echo("Load lazy from " .. lazypath)
 if not vim.loop.fs_stat(lazypath) then
-    lazy_install(lazypath)
+	lazy_install(lazypath)
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 if vim.g.vscode then
-    Echo "Loading nvim from vscode."
+	Echo("Loading nvim from vscode.")
 else
-    require('lazy').setup({
-        spec = {
-            { import = "plugins" },
-        }
-    })
-    require('core/lspconfig')
+	require("lazy").setup({
+		spec = {
+			{ import = "plugins" },
+		},
+	}, {
+		rocks = {
+			enabled = true,
+			hererocks = false,
+		},
+	})
+	require("core/lspconfig")
 end
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,45 +1,38 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
-    cmd = { "NvimTreeToggle", "NvimTreeFocus", "NvimTreeFindFile" },
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	cmd = { "NvimTreeToggle", "NvimTreeFocus", "NvimTreeFindFile" },
 
-    init = function()
-        -- Disable netrw
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
-        vim.keymap.set('n', '<leader>tt', ':NvimTreeToggle<CR>', { silent = true, desc = 'Nvim[T]ree [T]oggle' })
-        vim.keymap.set('n', '<leader>tf', ':NvimTreeFindFile<CR>', { silent = true, desc = 'Nvim[T]ree [F]ind File' })
-    end,
+	init = function()
+		-- Disable netrw
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+		vim.keymap.set("n", "<leader>tt", ":NvimTreeToggle<CR>", { silent = true, desc = "Nvim[T]ree [T]oggle" })
+		vim.keymap.set("n", "<leader>tf", ":NvimTreeFindFile<CR>", { silent = true, desc = "Nvim[T]ree [F]ind File" })
+	end,
 
-    opts = {
-        sort_by = "case_sensitive",
-        view = {
-            width = 38,
-        },
-        on_attach = function(buf)
-            local function opts(desc)
-                return { desc = "nvim-tree: " .. desc, buffer = buf, noremap = true, silent = true, nowait = true }
-            end
+	config = function()
+		require("nvim-tree").setup({
+			sort = {
+				sorter = "case_sensitive",
+			},
+			view = {
+				width = 38,
+			},
+		})
 
-            local api = require("nvim-tree/api")
-            api.config.mappings.default_on_attach(buf)
-            vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts('Up'))
-            vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
-        end,
-    },
-    config = function(_, opts)
-        require("nvim-tree").setup(opts)
+		local function auto_open_selected_file()
+			local buf = vim.api.nvim_get_current_buf()
+			local bufname = vim.api.nvim_buf_get_name(buf)
+			if vim.fn.isdirectory(bufname) or vim.fn.isfile(bufname) then
+				require("nvim-tree.api").tree.find_file(vim.fn.expand("%:p"))
+			end
+		end
 
-        local function auto_open_selected_file()
-            local buf = vim.api.nvim_get_current_buf()
-            local bufname = vim.api.nvim_buf_get_name(buf)
-            if vim.fn.isdirectory(bufname) or vim.fn.isfile(bufname) then
-                require("nvim-tree.api").tree.find_file(vim.fn.expand("%:p"))
-            end
-        end
-
-        vim.api.nvim_create_autocmd("BufEnter", { callback = auto_open_selected_file })
-    end,
+		vim.api.nvim_create_autocmd("BufEnter", { callback = auto_open_selected_file })
+	end,
 }
