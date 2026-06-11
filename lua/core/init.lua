c63eed86e40aa6fc3ee6388c5642447338f182c5
yiwen <jamesrily686@gmail.com>
2026-06-11 19:00:26 +0800

    remove nvim-treesitter

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -12,9 +12,9 @@ vim.o.backup = false
 vim.o.swapfile = false
 
 -- Clipboard
-vim.opt.clipboard = 'unnamedplus' -- use system clipboard
+vim.opt.clipboard = "unnamedplus" -- use system clipboard
 -- vim.opt.completeopt = { 'menu', 'menuon', 'noselect' }
-vim.opt.mouse = 'a'               -- allow the mouse to be used in nvim
+vim.opt.mouse = "a" -- allow the mouse to be used in nvim
 
 -- Tab
 vim.opt.tabstop = 4
@@ -24,28 +24,27 @@ vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "typescript",
-    callback = function()
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.tabstop = 2
-        vim.opt_local.softtabstop = 2
-    end
+	pattern = "typescript",
+	callback = function()
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.tabstop = 2
+		vim.opt_local.softtabstop = 2
+	end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "javascript",
-    callback = function()
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.tabstop = 2
-        vim.opt_local.softtabstop = 2
-    end
+	pattern = "javascript",
+	callback = function()
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.tabstop = 2
+		vim.opt_local.softtabstop = 2
+	end,
 })
-
 
 -- UI
 vim.opt.number = true
 vim.opt.relativenumber = false
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 vim.opt.cursorline = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
@@ -56,7 +55,7 @@ vim.opt.incsearch = true
 vim.opt.hlsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- undofile
 vim.opt.undofile = true
@@ -68,35 +67,40 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 vim.diagnostic.config({
-    virtual_lines = false,
-    virtual_text = true,
-    signs = {
-        text = {
-            [vim.diagnostic.severity.ERROR] = "",
-            [vim.diagnostic.severity.WARN]  = "",
-            [vim.diagnostic.severity.HINT]  = "󰟃",
-            [vim.diagnostic.severity.INFO]  = "",
-        },
-    },
-    update_in_insert = true,
-    serverity_sort = true,
-    underline = true,
-    float = {
-        border = "single",
-        source = true,
-        format = function(diagnostic)
-            return string.format(
-                "%s (%s) [%s]",
-                diagnostic.message,
-                diagnostic.source,
-                diagnostic.code or diagnostic.user_data.lsp.code
-            )
-        end
-    },
+	virtual_lines = false,
+	virtual_text = true,
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "",
+			[vim.diagnostic.severity.WARN] = "",
+			[vim.diagnostic.severity.HINT] = "󰟃",
+			[vim.diagnostic.severity.INFO] = "",
+		},
+	},
+	update_in_insert = true,
+	serverity_sort = true,
+	underline = true,
+	float = {
+		border = "single",
+		source = true,
+		format = function(diagnostic)
+			return string.format(
+				"%s (%s) [%s]",
+				diagnostic.message,
+				diagnostic.source,
+				diagnostic.code or diagnostic.user_data.lsp.code
+			)
+		end,
+	},
 })
 
-vim.opt.winborder = 'single'
+vim.opt.winborder = "single"
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
