0013732d29fdf8ce0d7f418cddaa1d3cb3bc3128
cgfork <jamesrily686@gmail.com>
2026-06-18 17:28:24 +0800

    current line blame open

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,23 +1,24 @@
 return {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-        require('gitsigns').setup {
-            signs = {
-                add          = { text = '┃' },
-                change       = { text = '┃' },
-                delete       = { text = '_' },
-                topdelete    = { text = '‾' },
-                changedelete = { text = '~' },
-                untracked    = { text = '┆' },
-            },
-            signs_staged = {
-                add          = { text = '┃' },
-                change       = { text = '┃' },
-                delete       = { text = '_' },
-                topdelete    = { text = '‾' },
-                changedelete = { text = '~' },
-                untracked    = { text = '┆' },
-            },
-        }
-    end
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup({
+			current_line_blame = true,
+			signs = {
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+			signs_staged = {
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+		})
+	end,
 }
